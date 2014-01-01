ed70f13700bf83a554aa136d28919cf35903f602
ralf <ralf@rouge.fritz.box>
2014-01-01 15:15:40 +0100

    Added ant file and test suite.
    Change-Id: I6efe7d0cece3f24a8cb99e5e02e564791e6022df

---
+++ src/TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import test.webservices.LeagueTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    LeagueTest.class
+})
+public class TestSuite {
+
+}
